1e31b8800e8647bc06d1798dd18557129f845047
Jonas Liljegren <jonas@liljegren.org>
2023-09-29 17:12:45 +0200

    command line args supported

--- sync_domain_ip.mjs
+++ sync_domain_ip.mjs
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 import * as HTTPS from "https"
-import * as DNS from "dns";
+import * as DNS from "dns"
 import {conf} from "./conf.mjs"
-import process from "process";
-//import { promisify } from "util";
+import process from "process"
+//import { promisify } from "util"
 
 
 const log = console.log.bind(console)
@@ -23,13 +23,18 @@ resolver.setServers(dns_ips)
 
 const is_main = import.meta.url.endsWith(process.argv[1])
 if( is_main ){
-	const res = await sync_domain_ip({dry:false,verbose:true,force:false});
-	log(res);
+	const argv = process.argv.slice(2)
+	const params = {}
+	for( const arg of ['dry','verbose','force'] )
+		params[arg] = argv.includes(arg)
+	
+	const res = await sync_domain_ip(params)
+	log(res)
 }
 
 export async function sync_domain_ip({dry,force,verbose}={}){
 	const [src_ip] = await resolver.resolve4(src_domain)
-	//const src_ip = "213.88.136.204";
+	//const src_ip = "213.88.136.204"
 
 	const [ref_ip] = await resolver.resolve4(ref_domain)
 
@@ -38,7 +43,7 @@ export async function sync_domain_ip({dry,force,verbose}={}){
 	//log("update", ref_ip, "=>", src_ip)
 	if(verbose) log({dry,force,ref_ip,src_ip})
 
-	let errcount = 0;
+	let errcount = 0
 
 	for( const dyndns of conf.dyndns ){
 		const user = dyndns.user
@@ -52,13 +57,13 @@ export async function sync_domain_ip({dry,force,verbose}={}){
 			const [cur_ip] = await resolver.resolve4(domain)
 			if( cur_ip === src_ip ){
 				if( verbose ) log("domain", domain, "unchanged")
-				continue;
+				continue
 			}
 			
-			const url = `https://${dyndns.server}?hostname=${domain}&myip=${src_ip}&wildcard=NOCHG`;
-			if( dry && verbose) log("would call", url);
-			if( dry ) continue;
-			const res = await basic_fetch(url, headers);
+			const url = `https://${dyndns.server}?hostname=${domain}&myip=${src_ip}&wildcard=NOCHG`
+			if( dry && verbose) log("would call", url)
+			if( dry ) continue
+			const res = await basic_fetch(url, headers)
 
 			if( verbose && res === "good" ) log(`${user} / ${domain} ${res}`)
 			if( res !== "good" ){
@@ -69,9 +74,9 @@ export async function sync_domain_ip({dry,force,verbose}={}){
 		
 	}
 	
-	if( errcount ) return "911";
+	if( errcount ) return "911"
 
-	return "good";
+	return "good"
 }
 
 function basic_fetch( url, headers ){
